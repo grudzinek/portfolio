5bd1cd8889ebd8097b862cad80612160429d9387
Paweł Grudziński <grudzin16@gmail.com>
2021-04-19 16:19:04 +0200

    add animations to skills

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ const baner = document.querySelector(".baner");
 const languages = document.querySelector(".languages");
 const skills = document.querySelector(".skills");
 const items = document.querySelectorAll(".item");
+const describe = document.querySelector(".describe");
+const fab = document.querySelectorAll(".fab");
 
 let skillsPosition = skills.getBoundingClientRect();
+
+let describePosition = describe.getBoundingClientRect();
 
 $(".banerArrow").on("click", function() {
     $("body, html").animate({
         scrollTop: skillsPosition.top
-    }, 2000)
+    }, 1000)
+});
+
+$(".skillsArrow").on("click", function() {
+    $("body, html").animate({
+        scrollTop: describePosition.top
+    }, 1000)
+});
+
+
+$(window).on("scroll", function () {
+
+if($(window).scrollTop() > skillsPosition.top - 150){
+    $(".fab").addClass("active");
+    $(".skillsH1").addClass("activeH1");
+}
 })
 
 
-let banerHeight =  baner.offsetHeight
-
-window.addEventListener("scroll", function(){
-    let scrollYPosition = window.pageYOffset;
-    items.forEach(element => {
-        let speed = element.dataset.speed;
-        element.style.transform = `translateY(${scrollYPosition * speed}px)`;
-    });
-
-
-})
